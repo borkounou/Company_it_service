7af5aef6c5d9fdb87c0a3b2e47ded252a0ff74da
=cnousrad <=cherif-hassan.nousradine@loria.fr>
2024-06-25 15:08:34 +0200

    editing the website

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -5,10 +5,38 @@ from fastapi.responses import HTMLResponse
 
 
 
+def https_url_for(request:Request, name:str, **path_params:any)->str:
+    http_url = request.url_for(name, **path_params)
+    https_url =str(http_url).replace("http", "https", 1)
+    return request.url_for(name, **path_params)
+
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="./static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+templates.env.globals["https_url_for"] = https_url_for
+
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request":request})
+
+
+@app.get("/about", response_class=HTMLResponse)
+async def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request":request})
+
+
+@app.get("/service", response_class=HTMLResponse)
+async def service(request: Request):
+    return templates.TemplateResponse("service.html", {"request":request})
+
+
+@app.get("/blog", response_class=HTMLResponse)
+async def blog(request: Request):
+    return templates.TemplateResponse("blog.html", {"request":request})
+
+
+@app.get("/detail", response_class=HTMLResponse)
+async def detail(request: Request):
+    return templates.TemplateResponse("detail.html", {"request":request})
